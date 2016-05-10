3dcb4f117f209a841bcfe1863d0f8f8afdf1ecf4
Jacques Rott <jacques.rott@gmail.com>
2016-05-10 15:18:03 +0200

    Remove absolute path and make them all relative

--- install.sh
+++ install.sh
@@ -2,11 +2,16 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-mkdir -p /Users/jacques/src /Users/jacques/txt
-ln -S /Users/jacques/Library/Preferences/com.googlecode.iterm2.plist com.googlecode.iterm2.plist
+mkdir -p ~/src ~/txt
+ln -S ~/Library/Preferences/com.googlecode.iterm2.plist com.googlecode.iterm2.plist
 
 ./brew.sh
 ./osx.sh
+./python.sh
+
+# install .vim with all the plugins
+git submodule update --init --recursive
+./.vim/install.sh
 
 # has to be currently at the end
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
--- python.sh
+++ python.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 sudo easy_install pip
 sudo pip install virtualenv
 sudo pip install virtualenvwrapper
