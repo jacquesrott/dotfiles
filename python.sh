96fa17f670080d37123b54886c5f95a37ce91e27
Jacques Rott <jacques.rott@gmail.com>
2016-05-10 16:18:45 +0200

    Fix python virtualenvwrapper installation on El Capitan

--- python.sh
+++ python.sh
@@ -2,4 +2,7 @@
 
 sudo easy_install pip
 sudo pip install virtualenv
-sudo pip install virtualenvwrapper
+
+sudo pip install pbr
+sudo pip install --no-deps stevedore
+sudo pip install --no-deps virtualenvwrapper
